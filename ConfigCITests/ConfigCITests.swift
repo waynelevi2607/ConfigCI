e6bb0a418dc10dcc2fe7e6aad9e6fefc104416ae
Khoa Vo T.A <waynelevi2607@gmail.com>
2020-10-29 10:06:59 +0700

    Change setUp and tearDown method in XCTest

--- ConfigCITests/ConfigCITests.swift
+++ ConfigCITests/ConfigCITests.swift
@@ -11,11 +11,13 @@ import XCTest
 
 class ConfigCITests: XCTestCase {
 
-    override func setUpWithError() throws {
+    override func setUp() {
+        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDownWithError() throws {
+    override func tearDown() {
+        super.tearDown()
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
